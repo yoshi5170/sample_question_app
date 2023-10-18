6fbb008e50197f37f332106c7f8354774d2aebb7
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-18 21:23:05 +0900

    質問ページへのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get 'static_pages/top'
-  resources :diagnosis_steps, only: [:show, :update]
+  resources :questions, only: [:index, :create] do
+    collection do
+      get :result
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
