d99d7634bf8aba7f502d0b2d3162058e5b8e7412
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-18 17:10:16 +0900

    テーブルの状態をリセット

--- db/migrate/20231015174543_create_diagnosis_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateDiagnosisQuestions < ActiveRecord::Migration[7.0]
-  def change
-    create_table :diagnosis_questions do |t|
-      t.string :question_text, null:false
-
-      t.timestamps
-    end
-  end
-end
