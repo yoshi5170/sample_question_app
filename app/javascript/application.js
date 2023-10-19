8d5f67601764bbefb368ad9053d8913848fa225f
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-19 16:17:44 +0900

    jsファイルを編集

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,18 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "@fortawesome/fontawesome-free/js/all";
 
 document.addEventListener("turbo:load", () => {
   let currentQuestion = 1;
   const totalQuestions = 10;
 
   const startButton = document.getElementById('start-diagnosis');
+  const startDescription = document.getElementById('start-description');
   startButton.addEventListener('click', () => {
     startButton.classList.add('hidden');
+    startDescription.classList.add('hidden');
+
     // 初めの質問を表示
     document.getElementById(`question_${currentQuestion}`).classList.remove('hidden');
   });
@@ -27,9 +31,9 @@ document.addEventListener("turbo:load", () => {
       }
 
       document.getElementById(`question_${currentQuestion}`).classList.add('hidden');
-      if (currentQuestion === totalQuestions - 1) {
+      if (currentQuestion === totalQuestions-1) {
         // 最後の質問なので、"次へ"ボタンを隠し、"回答する"ボタンを表示
-        event.target.classList.add('hidden');  // "次へ"ボタンを隠す
+        // event.target.classList.add('hidden');  // "次へ"ボタンを隠す
         document.getElementById('submit-button').classList.remove('hidden');  // "回答する"ボタンを表示
       }
 
