fe11427a89ff849f77bd9c7a82ae5ee2632aefb8
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-10-19 16:06:10 +0900

    カスタムカラーを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#4798e8',
+        customBlue2: '#00AFB9',
+        customBlue3: '#f39e9e',
+        customBlue4: '#FFFCF3',
+        customBlue5: '#6aaced',
+        customSky: '#c09f98',
+        customSky2: '#cc9b90',
+        customSky3: '#b18880',
+        customSky4: '#c08375',
+        customSky5: '#f5afaf',
+        customOrange3: '#FDFCDC',
+        customOrange2: '#FED9B7',
+        customOrange: '#f9ebeb',
+        customOrange4: '#ffcaa7',
+        customBrown: '#a77a00',
+        customBrown2: '#bb8800',
+        customBrown4: '#ffe7a7',
+        customBrown5: '#40241d',
+        customPurple: '#9e9ff3',
+      }
+    }
+  },
 }
